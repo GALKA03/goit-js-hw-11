e7e7dbc7ac2612ba5d808d953f3d685d2736ffba
Galina Matvienko <galyagmgm@gmail.com>
2022-10-23 12:29:45 +0200

    ;,

--- src/index.js
+++ src/index.js
@@ -12,32 +12,32 @@ import 'simplelightbox/dist/simple-lightbox.min.css'
    const btnLoadMore = document.querySelector('.load-more')
    
 
-function renderEventsPhoto(events) {
-    const marcup = events
-        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-            return  `
- <div class="photocard">
+// function renderEventsPhoto(events) {
+//     const marcup = events
+//         .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+//             return  `
+//  <div class="photocard">
 
- <a class="gallery__link" href="${largeImageURL}">
-       <img src ="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
-       <div class="info">
-           <p class="info-item">
-           <b>likes:${likes}</b>
-           </p>
-           <p class="info-item">
-               <b>views:${views}</b>
-           </p>
-<p class="info-item">
-               <b>comments:${comments}</b>
-           </p>
-           <p class="info-item">
-               <b>downloads:${downloads}</b>
-           </p>
-       </div>
-</div>    ` 
-    }).join('')
-    galleryConteiner.innerHTML = marcup;
-}
+//  <a class="gallery__link" href="${largeImageURL}">
+//        <img src ="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
+//        <div class="info">
+//            <p class="info-item">
+//            <b>likes:${likes}</b>
+//            </p>
+//            <p class="info-item">
+//                <b>views:${views}</b>
+//            </p>
+// <p class="info-item">
+//                <b>comments:${comments}</b>
+//            </p>
+//            <p class="info-item">
+//                <b>downloads:${downloads}</b>
+//            </p>
+//        </div>
+// </div>    ` 
+//     }).join('')
+//     galleryConteiner.innerHTML = marcup;
+// }
 
 
 
--- src/js/marcup.js
+++ src/js/marcup.js
@@ -1,27 +1,27 @@
 export {renderEventsPhoto}
-//     function renderEventsPhoto(events) {
-//     const marcup = events
-//         .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
-//             return  `
-//  <div class="photocard">
+    function renderEventsPhoto(events) {
+    const marcup = events
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return  `
+ <div class="photocard">
 
-//  <a class="gallery__link" href="${largeImageURL}">
-//        <img src ="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
-//        <div class="info">
-//            <p class="info-item">
-//            <b>likes:${likes}</b>
-//            </p>
-//            <p class="info-item">
-//                <b>views:${views}</b>
-//            </p>
-// <p class="info-item">
-//                <b>comments:${comments}</b>
-//            </p>
-//            <p class="info-item">
-//                <b>downloads:${downloads}</b>
-//            </p>
-//        </div>
-// </div>    ` 
-//     }).join('')
-//     refs.galleryConteiner.innerHTML = marcup;
-// }
+ <a class="gallery__link" href="${largeImageURL}">
+       <img src ="${webformatURL}" alt="${tags}" loading="lazy"  /></a>
+       <div class="info">
+           <p class="info-item">
+           <b>likes:${likes}</b>
+           </p>
+           <p class="info-item">
+               <b>views:${views}</b>
+           </p>
+<p class="info-item">
+               <b>comments:${comments}</b>
+           </p>
+           <p class="info-item">
+               <b>downloads:${downloads}</b>
+           </p>
+       </div>
+</div>    ` 
+    }).join('')
+    refs.galleryConteiner.innerHTML = marcup;
+}
